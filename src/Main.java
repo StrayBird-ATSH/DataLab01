a21c1a6afc7e2f009e313621424978a568d1eb56
StrayBird_ATSH <16307110064@fudan.edu.cn>
2018-09-12 16:53:59 +0800

    Changed the second method.

--- src/Main.java
+++ src/Main.java
@@ -24,52 +24,52 @@ public class Main {
         }
     }
 
-    //    @SuppressWarnings("rawtypes")
-    private static void mergeSort(@NotNull int[] list) {
-        //If list is empty; no need to do anything
-        if (list.length <= 1) {
+    private static void mergeSort(@NotNull int[] A) {
+//        Simply return terminate this function if the list
+//         meets the end
+        if (A.length <= 1)
             return;
-        }
 
-        //Split the array in half in two parts
-        int[] first = new int[list.length / 2];
-        int[] second = new int[list.length - first.length];
-        System.arraycopy(list, 0, first, 0, first.length);
-        System.arraycopy(list, first.length, second, 0, second.length);
 
-        //Sort each half recursively
-        mergeSort(first);
-        mergeSort(second);
+        //Divide the n-element sequence to be sorted into two sub-sequences
+//        of n/2 elements each
+        int[] left = new int[A.length / 2];
+        int[] right = new int[A.length - left.length];
+        System.arraycopy(A, 0, left, 0, left.length);
+        System.arraycopy(A, left.length, right, 0, right.length);
 
-        //Merge both halves together, overwriting to original array
-        merge(first, second, list);
+        //Sort the two sub-sequences recursively using merge sort
+        mergeSort(left);
+        mergeSort(right);
+
+        //Merge the two sorted sub-sequences to produce the sorted answer
+        merge(left, right, A);
     }
 
-    //    @SuppressWarnings({"rawtypes", "unchecked"})
-    private static void merge(@NotNull int[] first, int[] second, int[] result) {
-        //Index Position in first array - starting with first element
-        int iFirst = 0;
+    private static void merge(@NotNull int[] left, int[] right, int[] result) {
+        //Index Position in left array - starting with left element
+        int iForLeft = 0;
 
-        //Index Position in second array - starting with first element
-        int iSecond = 0;
+        //Index Position in right array - starting with left element
+        int iForRight = 0;
 
-        //Index Position in merged array - starting with first position
+        //Index Position in merged array - starting with left position
         int iMerged = 0;
 
-        //Compare elements at iFirst and iSecond,
+        //Compare elements at iForLeft and iForRight,
         //and move smaller element at iMerged
-        while (iFirst < first.length && iSecond < second.length) {
-            if (first[iFirst] - second[iSecond] < 0) {
-                result[iMerged] = first[iFirst];
-                iFirst++;
+        while (iForLeft < left.length && iForRight < right.length) {
+            if (left[iForLeft] - right[iForRight] < 0) {
+                result[iMerged] = left[iForLeft];
+                iForLeft++;
             } else {
-                result[iMerged] = second[iSecond];
-                iSecond++;
+                result[iMerged] = right[iForRight];
+                iForRight++;
             }
             iMerged++;
         }
         //copy remaining elements from both halves - each half will have already sorted elements
-        System.arraycopy(first, iFirst, result, iMerged, first.length - iFirst);
-        System.arraycopy(second, iSecond, result, iMerged, second.length - iSecond);
+        System.arraycopy(left, iForLeft, result, iMerged, left.length - iForLeft);
+        System.arraycopy(right, iForRight, result, iMerged, right.length - iForRight);
     }
 }
